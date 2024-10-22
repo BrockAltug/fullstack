ac7e6482656d0c825540151e9b7a8668aa92873e
Brock Altug <brock.altug99@gmail.com>
2024-10-21 19:48:53 -0700

    build image carousel with navigation and event control in Event-Bubbling project: cover DOM manipulation, event handling, and use of

--- web-apis/event-bubbling/assets/js/script.js
+++ web-apis/event-bubbling/assets/js/script.js
@@ -1,8 +1,14 @@
-// TODO: Which element is the following line of code selecting?
-const carousel = document.querySelector('.carouselbox');
-// TODO: Which element is the following line of code selecting?
-const next = carousel.querySelector('.next');
-const prev = carousel.querySelector('.prev');
+// TODO: which element is the following line of code selecting?
+const carousel = document.querySelector('.carouselbox'); 
+// this line is selecting the element with the class 'carouselbox' from the document
+
+// TODO: which element is the following line of code selecting?
+const next = carousel.querySelector('.next'); 
+// this line is selecting the element with the class 'next' that is a descendant of the 'carouselbox' element
+
+const prev = carousel.querySelector('.prev'); 
+// this line is selecting the element with the class 'prev' that is a descendant of the 'carouselbox' element
+
 let index = 0;
 let currentImage;
 
@@ -26,25 +32,30 @@ function navigate(direction) {
   carousel.style.backgroundImage = `url('${currentImage}')`;
 }
 
-// TODO: Describe the functionality of the following event listener.
+// TODO: describe the functionality of the following event listener.
 carousel.addEventListener('click', function () {
   window.location.href = images[index];
 });
+// this event listener redirects the user to the url of the current image when the carouselbox is clicked
 
-// TODO: Describe the functionality of the following event listener.
+// TODO: describe the functionality of the following event listener.
 next.addEventListener('click', function (event) {
-  // TODO: What is the purpose of the following line of code?
+  // TODO: what is the purpose of the following line of code?
   event.stopPropagation();
+  // this line prevents the click event from bubbling up to the parent elements, such as the carouselbox
 
   navigate(1);
 });
+// this event listener moves to the next image in the carousel when the 'next' element is clicked
 
-// TODO: Describe the functionality of the following event listener.
+// TODO: describe the functionality of the following event listener.
 prev.addEventListener('click', function (event) {
-  // TODO: What would happen if we didn't add the following line of code?
+  // TODO: what would happen if we didn't add the following line of code?
   event.stopPropagation();
+  // if we didn't add this line, the click event would propagate to the carouselbox and trigger its event listener, causing an unintended redirect
 
   navigate(-1);
 });
+// this event listener moves to the previous image in the carousel when the 'prev' element is clicked
 
 navigate(0);
