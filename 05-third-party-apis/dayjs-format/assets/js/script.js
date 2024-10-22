8bc5af9a94f490ebc59ef6f237a03469e50f8e09
Brock Altug <brock.altug99@gmail.com>
2024-10-21 20:20:42 -0700

    demonstrate date manipulation with Day.js in DayJS-Format project: cover date formatting, time display, and date difference calculations

--- 05-third-party-apis/dayjs-format/assets/js/script.js
+++ 05-third-party-apis/dayjs-format/assets/js/script.js
@@ -1,16 +1,27 @@
-// Use Day.js to format the date and assign to the declared variable.
+// Use Day.js to format the following variables:
 
-// TODO: 1. What is your graduation date in the following format: Jan 1, 1999?
+// 1. What is your graduation date in the following format: Jan 1, 1999?
+const gradDate = dayjs('2023-06-15').format('MMM D, YYYY');
+$('#1a').text(gradDate);
 
-// TODO: 2. What day of the week will 1/1/2027 be? (e.g. Is it "Monday"?)
+// 2. What day of the week will 1/1/2027 be? (e.g. Is it "Monday"?)
+const weekDay = dayjs('2027-01-01').format('dddd');
+$('#2a').text(weekDay);
 
-// TODO: 3. What is the current time in the format: hours:minutes:seconds
+// 3. What is the current time in the format: hours:minutes:seconds (12-hour clock)
+const time = dayjs().format('hh:mm:ss A');
+$('#3a').text(time);
 
-// TODO: 4. What is the current Unix timestamp?
+// 4. What is the current Unix timestamp?
+const unix = dayjs().unix();
+$('#4a').text(unix);
 
-// TODO: 5. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
+// 5. Parse the following Unix timestamp, 1318781876, and convert into any time/date format.
+const unixFormat = dayjs.unix(1318781876).format('MMM D, YYYY, hh:mm:ss A');
+$('#5a').text(unixFormat);
 
-// TODO: 6. What is the difference in days between May 4, 2027 and today? Hint:
-// You can display the difference between two dayjs objects by using the dayjs
-// diff method.)
-
+// 6. What is the difference in days between May 4, 2027 and today?
+const targetDay = dayjs('2027-05-04'); // dayjs object for May 4, 2027
+const today = dayjs(); // dayjs object for today
+const daysDifference = targetDay.diff(today, 'days'); // difference in days
+$('#6a').text(daysDifference);
