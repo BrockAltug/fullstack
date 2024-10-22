d5b935190c34b11732b890fd85df4a46a62cef09
Brock Altug <brock.altug99@gmail.com>
2024-10-21 19:47:19 -0700

    demonstrate dynamic creation and styling of HTML elements using JavaScript in Create-Append project: cover , , and inline CSS

--- web-apis/create-append/script.js
+++ web-apis/create-append/script.js
@@ -26,6 +26,11 @@ infoEl.appendChild(nameEl);
 body.appendChild(favoriteEl);
 // Append ordered list
 favoriteEl.appendChild(listEl);
+// Append list items to ordered list element
+listEl.appendChild(li1);
+listEl.appendChild(li2);
+listEl.appendChild(li3);
+listEl.appendChild(li4);
 
 h1El.setAttribute('style', 'margin:auto; width:50%; text-align:center;');
 infoEl.setAttribute('style', 'margin:auto; width:50%; text-align:center;');
@@ -35,3 +40,16 @@ kittenEl.setAttribute('style', 'font-size:25px; text-align:center;');
 favoriteEl.setAttribute('style', 'font-size:20px;');
 
 // TODO: Add ordered list items containing four favorite foods
+li1.textContent = 'Pizza';
+li2.textContent = 'Sushi';
+li3.textContent = 'Tacos';
+li4.textContent = 'Ice Cream';
+
+// Add styling to list element
+listEl.setAttribute('style', 'background:#333333; padding:20px;');
+
+// Add styling to list items
+li1.setAttribute('style', 'color:white; background: #666666; padding: 5px; margin-left: 35px;');
+li2.setAttribute('style', 'color:white; background: #777777; padding: 5px; margin-left: 35px;');
+li3.setAttribute('style', 'color:white; background: #888888; padding: 5px; margin-left: 35px;');
+li4.setAttribute('style', 'color:white; background: #999999; padding: 5px; margin-left: 35px;');
