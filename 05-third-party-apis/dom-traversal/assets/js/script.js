5207fd4649ba82d990f73c89e1986146862efcdb
Brock Altug <brock.altug99@gmail.com>
2024-10-21 20:21:22 -0700

    implement jQuery DOM traversal and manipulation in Dom-Traversal project: cover dynamic game board updates and efficient element selection

--- 05-third-party-apis/dom-traversal/assets/js/script.js
+++ 05-third-party-apis/dom-traversal/assets/js/script.js
@@ -1,6 +1,13 @@
+// Ensure the document is rendered when manipulating DOM elements
 // Assemble: Create/select DOM elements
 const rootEl = $('#root');
 
-// TODO: Starting from the root element, `rootEl`, make all the background color for each box white.
+// Starting from the root element, `<main id="root">`, select all the boxes and change the background color to white.
+rootEl.children('ul').children().css('background-color', 'white');
 
-// TODO: Starting from `rootEl`, create the jQuery statement that will add "O" to block the "X" from winning
+// Select the last `<ul>` or the third row.
+// Long traverals can be segmented to provide better readability.
+const rowThree = rootEl.children('ul').eq(2);
+
+// Select the children of the `<ul>`, then select the first box and add the 'O'.
+rowThree.children().eq(0).text('O');
