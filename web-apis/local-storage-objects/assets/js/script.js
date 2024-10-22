88ec42032fbc23eb349946b7dd3b10af465ef1d1
Brock Altug <brock.altug99@gmail.com>
2024-10-21 19:54:04 -0700

    demonstrate object creation and data storage in Local-Storage-Objects project: cover form handling, JSON conversion, and local storage usage

--- web-apis/local-storage-objects/assets/js/script.js
+++ web-apis/local-storage-objects/assets/js/script.js
@@ -8,6 +8,16 @@ signUpButton.addEventListener('click', function (event) {
   event.preventDefault();
 
   // TODO: Create user object from submission
+  //creates userData to store firstName, lastName, email, password into local storage
+    const userData = {
+      firstName : firstNameInput.value.trim(), //.trim removes the whitespace 
+      lastName : lastNameInput.value.trim(),
+      email : emailInput.value.trim(),
+      password : passwordInput.value.trim()
+    };
+  
 
   // TODO: Set new submission to local storage
+  localStorage.setItem("user", JSON.stringify(userData))
+  
 });
