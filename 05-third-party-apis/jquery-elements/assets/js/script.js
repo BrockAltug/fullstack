707655b47e89fc9691cf032edc298e7d09684b18
Brock Altug <brock.altug99@gmail.com>
2024-10-21 20:23:14 -0700

    build Quote Display Application with dynamic content creation in Quote Display Application project: cover jQuery DOM manipulation, styling, and hierarchical structure

--- 05-third-party-apis/jquery-elements/assets/js/script.js
+++ 05-third-party-apis/jquery-elements/assets/js/script.js
@@ -1,18 +1,28 @@
-// TODO: Select the existing DOM element, <div id="root">, and assign to a new variable
-// Hint: Visit the documentation https://api.jquery.com/
+// Select the existing DOM element, <div id="root">, and assign to a new variable
+const rootEl = $('#root');
 
-// TODO: Create a `<p>` element that will store an author's name and assign that created element to a new variable
+// Create a `<p>` element to store the quote's author and assign to a new variable
+const authorEl = $('<p>');
 
-// TODO: Add the following text to the newly created `<p>` element: "~ Carol Dweck"
+// Add text to the author element
+authorEl.text('~ Carol Dweck');
 
-// TODO: Add the class `plain` to the author element
+// Add the class `plain` to the author element
+authorEl.addClass('plain');
 
-// TODO: Create a new `<h1>` element that will store an author's quote and assign it to new variable
+// Create a `<h1>` element and assign to new variable
+const quoteEl = $('<h1>');
 
-// TODO: Add the following quote text to the quote element, "Love Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning."
+// Add a meaningful quote to the quote element
+quoteEl.text(
+  'Love Challenges, Be Intrigued by Mistakes, Enjoy Effort, and Keep Learning.'
+);
 
-// TODO: Apply the class `fancy` to the quote element
+// Apply the class `fancy` to the quote element
+quoteEl.attr('class', 'fancy');
 
-// TODO: Append the author element to the quote element
+// Attach the author element to the quote element
+quoteEl.append(authorEl);
 
-// TODO: Append the quote element to the HTML element with an ID of `root` in `index.html`
+// Append the quote element to the page
+rootEl.append(quoteEl);
