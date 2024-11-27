eaa9921f454e4eb2a98412f8c852c7f440277b04
Brock Altug <brock.altug99@gmail.com>
2024-11-26 21:31:56 -0800

    Implement reusable `findAverage` function using higher-order `.reduce()` for calculating averages

--- 13-cs/higher-order-functions/index.js
+++ 13-cs/higher-order-functions/index.js
@@ -1,6 +1,5 @@
 const grades = [56, 78, 99, 85];
 // TODO: Complete the 'findAverage' function below.
-// Complete the 'findAverage' function below.
 function findAverage(accumulator, currentValue, index, array) {
     accumulator += currentValue;
     // If it's the last element, divide by the total number of elements to calculate the average
@@ -10,7 +9,6 @@ function findAverage(accumulator, currentValue, index, array) {
     return accumulator; // Otherwise, just return the running total
 }
 // TODO: Pass the 'findAverage' into the reduce method.
-// Pass the 'findAverage' into the reduce method.
 let gradeAverage = grades.reduce(findAverage, 0);
 
 console.log(gradeAverage);
