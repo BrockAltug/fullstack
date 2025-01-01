3d5a67a57760918b955ce6ced78e7046abd37610
Brock Altug <brock.altug99@gmail.com>
2025-01-01 13:25:59 -0800

    implement modular routing in Express.js with organized API route files and middleware

--- 09-express-js/22-modular-routing/routes/tips.js
+++ 09-express-js/22-modular-routing/routes/tips.js
@@ -1,8 +1,33 @@
-const tips = require('express').Router();
+const tipsSubrouter = require("express").Router();
 // TODO: Import helper functions and dependencies
+const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const uuid = require("../helpers/uuid");
 
-// TODO: GET Route for retrieving all the tips
+// GET Route for retrieving all the tips
+tipsSubrouter.get("/", (req, res) => {
+  console.info(`${req.method} request received for tips`);
+  readFromFile("./db/tips.json").then((data) => res.json(JSON.parse(data)));
+});
 
-// TODO: POST Route for a new UX/UI tip
+// POST Route for a new UX/UI tip
+tipsSubrouter.post("/", (req, res) => {
+  console.info(`${req.method} request received to add a tip`);
 
-module.exports = tips;
+  const { username, topic, tip } = req.body;
+
+  if (username && topic && tip) {
+    const newTip = {
+      username,
+      tip,
+      topic,
+      tip_id: uuid(),
+    };
+
+    readAndAppend(newTip, "./db/tips.json");
+    res.json(`Tip added successfully`);
+  } else {
+    res.error("Error in adding tip");
+  }
+});
+
+module.exports = tipsSubrouter;
