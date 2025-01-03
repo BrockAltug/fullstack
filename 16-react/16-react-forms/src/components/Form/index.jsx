91e5de0f7712be31415b13d6a88574b0c27dfc64
Brock Altug <brock.altug99@gmail.com>
2025-01-03 01:15:47 -0800

    demonstrate React best practices with state management, props handling, event handling, and responsive design for a dynamic user interface

--- 16-react/16-react-forms/src/components/Form/index.jsx
+++ 16-react/16-react-forms/src/components/Form/index.jsx
@@ -1,45 +1,41 @@
 import { useState } from 'react';
 import './style.css';
 
-// Here we import a helper function that will check if the email is valid
+// Import helper functions to validate email and password
 import { checkPassword, validateEmail } from '../../utils/helpers';
 
 function Form() {
-  // Create state variables for the fields in the form
-  // We are also setting their initial values to an empty string
+  // Create state variables for email, username, password, and error message
   const [email, setEmail] = useState('');
   const [userName, setUserName] = useState('');
-  // TODO: Create a password variable and a function "setPassword" using useState
-
+  const [password, setPassword] = useState(''); // State for password
   const [errorMessage, setErrorMessage] = useState('');
 
   const handleInputChange = (e) => {
-    // Getting the value and name of the input which triggered the change
     const { target } = e;
     const inputType = target.name;
     const inputValue = target.value;
 
-    // Based on the input type, we set the state of either email, username, and password
-    // TODO: Add an else statement to the end that will set the password to the value of 'inputValue'
-
+    // Update state based on the input type
     if (inputType === 'email') {
       setEmail(inputValue);
     } else if (inputType === 'userName') {
       setUserName(inputValue);
+    } else if (inputType === 'password') { // Update password state
+      setPassword(inputValue);
     }
   };
 
   const handleFormSubmit = (e) => {
-    // Preventing the default behavior of the form submit (which is to refresh the page)
     e.preventDefault();
 
-    // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+    // Validate email and username
     if (!validateEmail(email) || !userName) {
       setErrorMessage('Email or username is invalid');
-      // We want to exit out of this code block if something is wrong so that the user can correct it
       return;
-      // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
     }
+
+    // Validate password
     if (!checkPassword(password)) {
       setErrorMessage(
         `Choose a more secure password for the account: ${userName}`
@@ -47,11 +43,11 @@ function Form() {
       return;
     }
 
-    // If successful, we want to clear out the input after registration.
+    // Clear input fields upon successful submission
     setUserName('');
-    // TODO: Set the password back to an empty string after the user clicks submit
-
+    setPassword(''); // Reset password to an empty string
     setEmail('');
+    setErrorMessage('');
     alert(`Hello ${userName}`);
   };
 
@@ -73,11 +69,15 @@ function Form() {
           type="text"
           placeholder="username"
         />
-        {/* TODO Add another input field with a value, name, type, and placeholder of "password" */}
-        {/* TODO Add a `onChange` attribute with a value of `handleInputChange` */}
-        <button type="submit">
-          Submit
-        </button>
+        {/* Password input field */}
+        <input
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+          type="password"
+          placeholder="password"
+        />
+        <button type="submit">Submit</button>
       </form>
       {errorMessage && (
         <div>
