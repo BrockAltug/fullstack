f20936704eb0cbae497a8150773da4238b5b345c
Brock Altug <brock.altug99@gmail.com>
2025-01-07 15:26:32 -0800

    Integrate Handlebars templates and frontend logic for Dish Management System
    - Developed Handlebars templates for dynamic rendering of dishes with conditional logic (e.g., ).
    - Implemented MVC architecture to separate concerns between Models, Views, and Controllers.
    - Added Sequelize models to manage database operations for dishes, including creation and updates.
    - Built RESTful routes for adding and editing dishes with backend integration.
    - Integrated client-side JavaScript for handling user interactions and sending API requests to the server.
    - Provided forms for adding and updating dishes with live updates to the frontend.
    - Enhanced the codebase for scalability and maintainability by adhering to the MVC paradigm.

--- 12-model-view-controller/10-handlebars-fe-logic/controllers/api/dish-routes.js
+++ 12-model-view-controller/10-handlebars-fe-logic/controllers/api/dish-routes.js
@@ -16,9 +16,11 @@ router.post('/', async (req, res) => {
   }
 });
 
-// TODO: According to MVC, what is the role of this action method?
+// ? According to MVC, what is the role of this action method?
+// ? This action method is the Controller. It accepts input and sends data to the Model and the View.
 router.put('/:id', async (req, res) => {
-  // TODO: Where is this action method sending the data from the body of the fetch request? Why?
+  // ? Where is this action method sending the data from the body of the fetch request? Why?
+  // ? It is sending the data to the Model so that one dish can be updated with new data in the database.
   try {
     const dish = await Dish.update(
       {
@@ -33,7 +35,8 @@ router.put('/:id', async (req, res) => {
         },
       }
     );
-    // TODO: If the database is updated successfully, what happens to the updated data below? What shape is dish?
+    // ? If the database is updated successfully, what happens to the updated data below?
+    // ? The updated data (dish) is then sent back to handler that dispatched the fetch request.
     res.status(200).json(dish);
   } catch (err) {
     res.status(500).json(err);
